bc63ba18781060f96a5ff3c78998b8068e47673f
nicholaskotzias <nicokotzias@gmail.com>
2025-04-14 19:14:17 -0300

    fix: corrigir o nome da coluna de 'email' para 'senha' no migration

--- database/migrations/2025_04_14_215906_create_alunos_table.php
+++ database/migrations/2025_04_14_215906_create_alunos_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('nome');
             $table->string('cpf');
             $table->string('email');
-            $table->string('email');
+            $table->string('senha');
             $table->softDeletes();
             $table->timestamps();
         });
